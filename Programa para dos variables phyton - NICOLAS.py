089a14986d931cc9c4ae2334e38786f12a5e223b
NICO VARGAS <nicolasvargasangarita07@gmail.com>
2025-02-18 07:26:24 -0500

    Create Programa para dos variables phyton - NICOLAS.py

---
+++ Programa para dos variables phyton - NICOLAS.py
@@ -0,0 +1,9 @@
+# SOLICITAR DOS NUMEROS AL USUARIO 
+Num1 = int(input("a"))
+Num2 = int(input("b"))
+
+print(Num1 + Num2)
+print(Num1 - Num2)
+print(Num1 * Num2)
+print(Num1 / Num2)
+print(Num1 % Num2)
