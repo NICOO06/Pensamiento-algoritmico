dd9923c24b50a67a7845c1b5144737fcf0f31fff
NICO VARGAS <nicolasvargasangarita07@gmail.com>
2025-02-18 07:32:19 -0500

    Create descuento del 15% - NICOLAS.py

---
+++ descuento del 15% - NICOLAS.py
@@ -0,0 +1,5 @@
+# solicitar el precio del articulo al usuario 
+precio = int(input("Ingresar el precio del articulo"))
+descuento = 15
+"monto_desceunto =(descuento /100) * precio"
+print(precio *0.85)
